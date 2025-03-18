0c3e49d49a0ba16a3b1b48c6b8eb2920faf91ecd
Just_Ilnaz <ayelion16@gmail.com>
2025-03-18 14:33:58 +0300

    Add files via upload

---
+++ Static/Static/Building.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal class Building 
+    {
+        // это айди
+        private static int lastBuildingNumber = 0;
+
+        
+        public int BuildingNumber { get; private set; }
+        public double Height { get; set; }
+        public int Floors { get; set; }
+        public int Apartments { get; set; }
+        public int Entrances { get; set; }
+
+        
+        public Building(double height, int floors, int apartments, int entrances)
+        {
+            BuildingNumber = ++lastBuildingNumber; // Генерация id
+            Height = height;
+            Floors = floors;
+            Apartments = apartments;
+            Entrances = entrances;
+        }
+
+        
+        public double GetFloorHeight() => Height / Floors;
+
+        public int GetApartmentsPerEntrance() => Apartments / Entrances;
+
+        public int GetApartmentsPerFloor() => Apartments / (Floors * Entrances);
+
+        
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Здание #{BuildingNumber}:");
+            Console.WriteLine($"  Высота: {Height} м");
+            Console.WriteLine($"  Этажей: {Floors}");
+            Console.WriteLine($"  Квартир: {Apartments}");
+            Console.WriteLine($"  Подъездов: {Entrances}");
+            Console.WriteLine($"  Высота этажа: {GetFloorHeight():F2} м");
+            Console.WriteLine($"  Квартир в подъезде: {GetApartmentsPerEntrance()}");
+            Console.WriteLine($"  Квартир на этаже: {GetApartmentsPerFloor()}");
+        }
+    }
+}
+
---
+++ Static/Static/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building1 = new Building(30.5, 10, 100, 5);
+            building1.PrintInfo();
+
+            Console.WriteLine();
+
+            Building building2 = new Building(50.0, 20, 200, 4);
+            building2.PrintInfo();
+        }
+    }
+}
